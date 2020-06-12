dc3f0c3b3df7a4b07ad951ab268f7a2f5581a1cf
Sven Clement <clement@clement-weyer.lu>
2020-06-12 20:19:29 +0200

    first commit

---
+++ lib/adt/driver.rb
@@ -0,0 +1,8 @@
+require "adt/driver/version"
+
+module Adt
+  module Driver
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/adt/driver/version.rb
@@ -0,0 +1,5 @@
+module Adt
+  module Driver
+    VERSION = "0.1.0"
+  end
+end
